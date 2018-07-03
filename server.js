6d28a5c816ab5aab6fd51be9fa5d25052e507d74
Kate Clawson <kclaw98@gmail.com>
2018-07-03 11:28:40 +0100

    style

--- server.js
+++ server.js
@@ -13,10 +13,28 @@ const compliments = [
     "You've learned a lot of things, and that's pretty hard to do"
 ];
 
+const insults = [
+    "You don't like nice today",
+    "That dress doens't look nice on you",
+    "When was the last time you worked out?",
+    "You can't do hard things",
+    "You've haven't gotten very far in this course. You're not very smart",
+    "You're barely programming! Not very impressive.",
+    "I'm really disappointed in you",
+    "You didn't make this",
+    "You've barely learned anything, and that's pretty hard to do"
+];
+
 // gets a random compliment from the array
 function getRandomCompliment() {
     const randomIndex = Math.floor(Math.random() * compliments.length);
     return compliments[randomIndex];
+}
+
+// gets a random compliment from the array
+function getRandomInsult() {
+    const randomIndex = Math.floor(Math.random() * insults.length);
+    return insults[randomIndex];
 }
 
 const app = express();
@@ -26,10 +44,22 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "style.css"));
+});
+
 app.get("/compliment", function(req, res) {
     res
         .json({
             compliment:getRandomCompliment()
+        })
+        .end();
+});
+
+app.get("/insult", function(req, res) {
+    res
+        .json({
+            insult:getRandomInsult()
         })
         .end();
 });
